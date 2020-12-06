86daeae149d518c6c3464a26ddf43a61e7636d95
Matt Travi <programmer@travi.org>
2020-12-05 22:30:43 -0600

    test(cucumber): silenced the publish banner

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '--require-module @babel/register --format-options \'{"snippetInterface": "async-await"}\'',
+  base: '--require-module @babel/register --format-options \'{"snippetInterface": "async-await"}\' --publish-quiet',
   wip: '--tags "@wip"',
   noWip: '--tags "not @wip"',
   focus: '--tags @focus'
